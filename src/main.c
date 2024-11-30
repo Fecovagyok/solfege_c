9e057087f65de99278fa8d488ba346558f675e9f
Fecovagyok <kistutu234@gmail.com>
2024-11-30 13:20:21 +0100

    Better message

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main(void) {
   enum Note start_note = Note_C;
   enum Interval interval = Minor_Second;
   enum Note new_one = note_interval(start_note, interval);
-  printf("Alaphang: %s + %s Kiszamolt: %s\n", note_to_string(start_note),
+  printf("%s + %s = %s\n", note_to_string(start_note),
          interval_to_string(interval), note_to_string(new_one));
   return 0;
 }
